238a43f8450cf3f48f0aa4e2ff226f0aa9ed68e8
piotrmozdzen <piotr.mozdzen@protonmail.com>
2025-06-05 23:54:58 +0200

    Initial commit with proper .gitignore

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-2 flex-col items-center justify-between p-24">
+      <h1>HelloWorld</h1>
+    </div>
+  );
+}
